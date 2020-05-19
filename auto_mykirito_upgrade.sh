4868adfbc655e408cbdfd65df03fa2cd72a35ed2
AustinComet <32216861+AustinComet@users.noreply.github.com>
2020-05-19 23:20:00 +0800

    upgrade file

--- auto_mykirito_upgrade.sh
+++ auto_mykirito_upgrade.sh
@@ -4,8 +4,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export  PATH
 
 main(){
-    cp /mnt/c/Users/f1302/Desktop/auto_mykirito_action.sh /home/a/Documant/auto_mykirito_action.sh
-    bash /home/a/Documant/auto_mykirito_action.sh
+    cp /mnt/d/TestCode/shellscript/automykirito/auto_mykirito_action.sh /home/a/Documant/automykirito/auto_mykirito_action.sh
+    cp /mnt/d/TestCode/shellscript/automykirito/auto_mykirito_attack.sh /home/a/Documant/automykirito/auto_mykirito_attack.sh
+    echo 'file upgrade done'
     return 0
 }
 
