e300ff9089b7cd42cadd9703e64f44362eb85b31
AustinComet <32216861+AustinComet@users.noreply.github.com>
2020-05-19 22:16:17 +0800

    test

--- auto_mykirito_attack.sh
+++ auto_mykirito_attack.sh
@@ -21,21 +21,21 @@ attack(){
             -H $'Accept: application/json, text/plain, */*' \
             -H $'Accept-Language: zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3' \
             -H $'Accept-Encoding: gzip, deflate' \
-            -H $"token: ${user}" \
+            -H $'token: 5ec20f7fe64003000a614820.D6gLTIXi3Y1621433153491.105926074920423477125' \
             -H $'Content-Type: application/json;charset=utf-8' \
             -H $'Content-Length: 69' \
             -H $'Origin: https://mykirito.com' \
             -H $'Connection: close' \
-            -H $"Referer: https://mykirito.com/profile/${attackuser}" \
+            -H $'Referer: https://mykirito.com/profile/5ec0d3d545eadc000a0d72bc' \
             -H $'Cookie: __cfduid=d41084fb786e67349cb642c917c54b29b1589897113; G_ENABLED_IDPS=google; G_AUTHUSER_H=0' \
             -b $'__cfduid=d41084fb786e67349cb642c917c54b29b1589897113; G_ENABLED_IDPS=google; G_AUTHUSER_H=0' \
-            --data-binary $"{\"type\":0,\"opponentUID\":\"${attackuser}\",\"shout\":\"\",\"lv\":${attackuserlevel}}" \
+            --data-binary $'{\"type\":0,\"opponentUID\":\"5ec0d3d545eadc000a0d72bc\",\"shout\":\"\",\"lv\":9}' \
             $'https://mykirito.com/api/challenge'
             ;;
         "1")
             curl -i -s -k  -X $'POST' \
             -H $'Host: mykirito.com' \
-            -H $'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0' \
+            -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0' \
             -H $'Accept: application/json, text/plain, */*' \
             -H $'Accept-Language: zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3' \
             -H $'Accept-Encoding: gzip, deflate' \
@@ -44,9 +44,9 @@ attack(){
             -H $'Content-Length: 69' \
             -H $'Origin: https://mykirito.com' \
             -H $'Connection: close' \
-            -H $"Referer: https://mykirito.com/profile/${attackuser}" \
-            -H $'Cookie: __cfduid=d41084fb786e67349cb642c917c54b29b1589897113; G_ENABLED_IDPS=google; G_AUTHUSER_H=0' \
-            -b $'__cfduid=d41084fb786e67349cb642c917c54b29b1589897113; G_ENABLED_IDPS=google; G_AUTHUSER_H=0' \
+            -H $"Referer: https://mykirito.com/profile/\"${attackuser}\"" \
+            -H $'Cookie: __cfduid=ddedfc675485a5bf044726373853f27011589797807; G_ENABLED_IDPS=google; G_AUTHUSER_H=0' \
+            -b $'__cfduid=ddedfc675485a5bf044726373853f27011589797807; G_ENABLED_IDPS=google; G_AUTHUSER_H=0' \
             --data-binary $"{\"type\":1,\"opponentUID\":\"${attackuser}\",\"shout\":\"\",\"lv\":${attackuserlevel}}" \
             $'https://mykirito.com/api/challenge'
             ;;
